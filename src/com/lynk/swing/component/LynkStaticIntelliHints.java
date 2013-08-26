e2afa314d66d5a16988997b222be4e5d894458e7
LynkLin@gmail.com <LynkLin@gmail.com>
2013-08-26 17:03:34 +0800

    Update.

--- src/com/lynk/swing/component/LynkStaticIntelliHints.java
+++ src/com/lynk/swing/component/LynkStaticIntelliHints.java
@@ -1,20 +1,41 @@
 package com.lynk.swing.component;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 public class LynkStaticIntelliHints extends LynkIntelliHints {
 	
 	private List<?> items;
 	
 	public static void install(JTextField textField, List<?> items) {
-		new LynkStaticIntelliHints(textField, items);
+		new LynkStaticIntelliHints(textField, items, -1);
 	}
 	
-	private LynkStaticIntelliHints(JTextField textField, List<?> items) {
-		super(textField);
+	public static void install(JTextField textField, List<?> items, int comboBoxNum) {
+		new LynkStaticIntelliHints(textField, items, comboBoxNum);
+	}
+	
+	private LynkStaticIntelliHints(JTextField textField, List<?> items, int comboBoxNum) {
+		super(textField, comboBoxNum);
 		this.items = items;
+		textField.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				super.mouseClicked(e);
+				if(SwingUtilities.isLeftMouseButton(e) && e.getClickCount() == 2) {
+					model.removeAllElements();
+					for(Object item: LynkStaticIntelliHints.this.items) {
+						model.addElement(item);
+					}
+					setPopupVisible(true);
+				}
+			}
+		});
 	}
 	
 	@Override
@@ -23,7 +44,7 @@ public class LynkStaticIntelliHints extends LynkIntelliHints {
 		String input = getText();
 		if (!input.isEmpty()) {
 			for (Object item : items) {
-				if (item.toString().toLowerCase().startsWith(input.toLowerCase())) {
+				if (item.toString().toLowerCase().contains(input.toLowerCase())) {
 					model.addElement(item);
 				}
 			}
